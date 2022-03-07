da1b74fa2f1e3c6e71eb56c4da4ffe0ac1baf073
Nirmit Sawhney <nirmitsawhney01@gmail.com>
2022-03-07 21:56:37 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app=express();
-const api="api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=487d8110cba67e50afc9b4200b40c7be";
+const api="https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=487d8110cba67e50afc9b4200b40c7be";
 import fetch from "node-fetch";
 
 app.use(express.json());
